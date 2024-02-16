278bb763d00af352b2acbc24a23a64ab9ca4d659
fallenleavesguy <9261562+fallenleavesguy@users.noreply.github.com>
2024-02-17 01:01:56 +0800

    Initial Commit

---
+++ stackviews/stackviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  stackviewsApp.swift
+//  stackviews
+//
+//  Created by donghs on 2024/2/17.
+//
+
+import SwiftUI
+
+@main
+struct stackviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
