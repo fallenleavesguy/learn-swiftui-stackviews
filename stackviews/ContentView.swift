132f80c718d756bcb9b073a74550c452a241279d
fallenleavesguy <9261562+fallenleavesguy@users.noreply.github.com>
2024-02-17 16:15:27 +0800

    basic

--- stackviews/ContentView.swift
+++ stackviews/ContentView.swift
@@ -9,16 +9,71 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            VStack() {
+                Text("Instant Developer")
+                    .fontWeight(.medium)
+                    .font(.system(size: 40))
+                    .foregroundStyle(.indigo)
+                Text("Get help from experts in 15 minutes")
+            }
+            HStack(alignment: .bottom, spacing: 10) {
+                Image("user1")
+                    .resizable()
+                .scaledToFit()
+                
+                Image("user2")
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("user3")
+                    .resizable()
+                    .scaledToFit()
+            }
+            .padding(.horizontal, 20)
+            
+            Text("Need help with coding problems? Register!")
+            
+            Spacer()
+            
+            VSignUpButtonGroup()
         }
-        .padding()
+//        .padding(.top, 30)
     }
 }
 
-#Preview {
+#Preview("IPhone") {
     ContentView()
+}
+
+#Preview("IPhone(Landscape)", traits: .landscapeRight) {
+    ContentView()
+}
+
+struct VSignUpButtonGroup: View {
+    var body: some View {
+        VStack() {
+            Button {
+                
+            } label: {
+                Text("Sign up")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.indigo)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            Button {
+                
+            } label: {
+                Text("Log In")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.gray)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+    }
 }
